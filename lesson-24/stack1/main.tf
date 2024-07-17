13c6438cc65670f7a3205adaeed3e671c5278a4f
Nikita Sizov <sizovns.13@gmail.com>
2024-07-17 13:42:31 +0300

    Added vpc2 with merge tags

--- lesson-24/stack1/main.tf
+++ lesson-24/stack1/main.tf
@@ -37,9 +37,9 @@ resource "aws_vpc" "vpc1" {
 
 
 resource "aws_vpc" "vpc2" {
-    cidr_block = "10.0.0.0/16"
-    tags = merge(local.common_tags, {
-        Name = "Stack1-VPC2"
-        Company = local.company_name
-    })
+  cidr_block = "10.0.0.0/16"
+  tags = merge(local.common_tags, {
+    Name    = "Stack1-VPC2"
+    Company = local.company_name
+  })
 }
